4ebdba55aa048bbcb4bca9d9ac2ff06f3153c8cb
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2021-04-24 13:38:37 +0200

    added numpy forgot it

--- pyelegant/report/plottools.py
+++ pyelegant/report/plottools.py
@@ -1,6 +1,8 @@
 import matplotlib
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from matplotlib.animation import FuncAnimation
 from matplotlib.colors import LinearSegmentedColormap
 
