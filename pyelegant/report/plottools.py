c940bed31bbb3561dd642b6ba03cf170f989be5a
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2021-04-24 15:52:56 +0200

    removed seaborn dep

--- pyelegant/report/plottools.py
+++ pyelegant/report/plottools.py
@@ -9,7 +9,8 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
+
+# import seaborn as sns
 from dask import dataframe as dd
 from matplotlib import cm
 from matplotlib.animation import FuncAnimation
